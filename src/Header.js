3f59ecdd1d50c8a79bd26bc4fe9da16435701172
Houleye <houleys843@gmail.com>
2026-05-02 21:53:12 +0000

    Lab 2 :composants statiques LigneBus et ListeLignes

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header-titre"> SénTransport</h1>
+      <p className="header-sous-titre">Réseau de transport Dakar Dem Dikk</p>
+    </header>
+  );
+}
+
+export default Header;
